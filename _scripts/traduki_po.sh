6cac2ded90674ec896ebe6b27b932cc4d5283ddc
jean <jean2@jcmtmarsac.zapto.org>
2020-09-03 16:38:17 +0200

    renomu trad_draft to trad_proj

--- _scripts/traduki_po.sh
+++ _scripts/traduki_po.sh
@@ -4,6 +4,10 @@
 #################################################################
 
 src=eo
+if [ x$TRADUKI_SRC != x ]
+then
+  src=$TRADUKI_SRC
+fi
 
 #FIC=locale/en/LC_MESSAGES/motioneye.po
 FIC=$1
